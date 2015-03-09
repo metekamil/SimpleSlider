5c09dae020e51a4357c8ffce17546b75876bb464
Mete Kamil <mete@metekamil.com>
2015-03-08 23:43:44 -0500

    Added direction methods

--- SimpleSlider.js
+++ SimpleSlider.js
@@ -1,8 +1,8 @@
 
  
 var gameIdx=0; 
-var games=["Street Fighter","Shinobi"];
-var gamesImg=["http://console.com.ua/images/stories/greview/sf4walk/street-fighter-4-all.jpg","http://fc02.deviantart.net/fs71/f/2012/106/0/a/hotsuma_from_shinobi_by_mcflight-d4wg60u.jpg"];
+var games=["Street Fighter","Shinobi","Call of Duty"];
+var gamesImg=["http://console.com.ua/images/stories/greview/sf4walk/street-fighter-4-all.jpg","http://gearnuke.com/wp-content/uploads/2014/07/Call-of-Duty-Advanced-Warfare-feature-image-3.jpg","http://fc02.deviantart.net/fs71/f/2012/106/0/a/hotsuma_from_shinobi_by_mcflight-d4wg60u.jpg"];
   
 function setGame(){
  
@@ -11,11 +11,16 @@ function setGame(){
     
 }
 
-function nextGame()
+function changeSlider(step)
 { 
-    gameIdx++; 
-    if (gameIdx>=2) {
+    gameIdx=gameIdx+step; 
+    if (gameIdx>=games.length) {
         gameIdx=0; 
     }
+    else{ 
+    	if (gameIdx<0) {
+        gameIdx=games.length-1; 
+    	}
+	}
     setGame();
 }
